bcf0b52d8590c06e04158239f4bba96920ae15e6
konstsem <konstantin.semenov.2@gmail.com>
2020-03-14 23:51:11 +0300

    feat: add sliding and images moving

--- script.js
+++ script.js
@@ -8,11 +8,17 @@ navItems.forEach((item) => {
   });
 });
 
+const moveImages = () => {
+  const images= document.querySelector('.images__wrapper');
+  images.prepend(images.lastElementChild);
+};
+
 const tags = document.querySelectorAll('.tag');
 tags.forEach((tag) => {
   tag.addEventListener('click', () => {
     resetSelected(tags, 'tag_selected');
     tag.classList.add('tag_selected');
+    moveImages();
   });
 });
 
@@ -23,3 +29,52 @@ images.forEach((image) => {
     image.classList.add('selected');
   })
 })
+
+let slides = document.querySelectorAll('.slider__slide');
+let currentItem = 0;
+let isEnabled = true;
+
+const changeCurrentItem = (n) => {
+  currentItem = (n + slides.length) % slides.length;
+}
+
+function hideItem(direction) {
+  isEnabled = false;
+  slides[currentItem].classList.add(direction);
+  slides[currentItem].addEventListener('animationend', function() {
+    this.classList.remove('active', direction);
+  });
+}
+
+function showItem(direction) {
+  slides[currentItem].classList.add('next', direction);
+  slides[currentItem].addEventListener('animationend', function() {
+    this.classList.remove('next', direction);
+    this.classList.add('active');
+    isEnabled = true;
+  });
+}
+
+function previousItem(n) {
+  hideItem('to-right');
+  changeCurrentItem(n - 1);
+  showItem('from-left');
+}
+
+function nextItem(n) {
+  hideItem('to-left');
+  changeCurrentItem(n + 1);
+  showItem('from-right');
+}
+
+document.querySelector('.control_left').addEventListener('click', function() {
+  if(isEnabled) {
+    previousItem(currentItem);
+  }
+});
+
+document.querySelector('.control_right').addEventListener('click', function() {
+  if(isEnabled) {
+    nextItem(currentItem);
+  }
+});
